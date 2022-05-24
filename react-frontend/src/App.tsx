85de806c1bf554a78ea5f4ce3abbfffdc98ea799
Alex <Wilmer.DelgadoAlama09@gmail.com>
2022-05-24 14:10:27 -0500

    test basic websocket connection and send message

--- react-frontend/src/App.tsx
+++ react-frontend/src/App.tsx
@@ -1,11 +1,31 @@
 import { TablePersons } from "@/components/common/TablePersons";
 
 function App() {
-  return (
-    <div className="App">
-     <TablePersons />
-    </div>
-  );
+	const ws = new WebSocket("ws://localhost:8080");
+
+	const sendToWS = (data: any) => {
+		ws.send(JSON.stringify(data));
+	};
+
+	ws.onopen = (e: any) => {
+		sendToWS([
+			"get-all-online",
+			{
+				dato: "dato",
+			},
+		]);
+	};
+
+	ws.onmessage = function (event: any) {
+		const json = JSON.parse(event.data);
+		console.log(json);
+	};
+
+	return (
+		<div className="App">
+			<TablePersons />
+		</div>
+	);
 }
 
 export default App;
